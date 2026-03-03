0a460ea57e6e0270ae6d02f7e4091dfff58d867d
skogix <emil@skogsund.se>
2026-03-03 10:44:41 +0100

    /home/skogix/claude auto-sync 10:44:41

---
+++ scripts/generate-git-commit-message.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+TS=$(date +%H:%M:%S)
+echo "/home/skogix/claude auto-sync $TS"
---
+++ scripts/git-auto-commit.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+gitwatch watch /home/skogix/claude \
+  --commit-message-script=/home/skogix/claude/scripts/generate-git-commit-message.sh \
+  --commit-on-start=true \
+  --debounce-seconds=1 \
+  --log-level=trace \
+  --remote=origin \
+  --watch=true >>/tmp/skogai-git-notify.claude.service.log 2>&1 &
